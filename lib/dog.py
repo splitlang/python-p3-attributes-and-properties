0566dd57756b9a691f33e2accd9d87d9dceb1fd8
splitlang <seanmark.student@moringaschool>
2024-06-18 00:15:30 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
+approved_breeds = ["Mastiff", "Chihuahua", "Corgi", "Shar Pei", "Beagle", "French Bulldog", "Pug", "Pointer"]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Pug"):
+        self._name = None
+        self._breed = None
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+    def bark(self):
+        print("Woof!")
